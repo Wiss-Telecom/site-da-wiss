0716827f27b720c756c44e96ade1329fd2d8b0a9
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-07-01 20:46:43 -0300

    first commit

---
+++ pages/api/contact.ts
@@ -0,0 +1,38 @@
+require('dotenv').config()
+import { NextApiRequest, NextApiResponse } from 'next'
+import nodemailer from 'nodemailer'
+
+export default function mailer (req: NextApiRequest, res: NextApiResponse) {
+  const EMAIL_HOST = process.env.EMAIL_HOST
+  const EMAIL_ACCOUNT = process.env.EMAIL_ACCOUNT
+  const PASSWORD = process.env.EMAIL_PASSWORD
+
+  const transporter = nodemailer.createTransport({
+    host: EMAIL_HOST,
+    port: 465,
+    secure: true,
+    auth: {
+      user: EMAIL_ACCOUNT,
+      pass: PASSWORD
+    }
+  })
+
+  const mailData = {
+    from: `${req.body.email}`,
+    to: 'thiago@wisstelecom.com.br',
+    subject: `Mensagem recebida no site de ${req.body.name}`,
+    text: req.body.message,
+    html: `
+      <p>Mensagem de: ${req.body.name}</p>
+      <p>Responder para: ${req.body.email}</p>
+      <div>${req.body.message}</div>
+    `
+  }
+
+  transporter.sendMail(mailData, (err, info) => {
+    if (err) console.log(err)
+    else console.log(info)
+  })
+  console.log('reached')
+  return res.status(200)
+}
