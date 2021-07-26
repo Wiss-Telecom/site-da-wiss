03b6e09fae9e3c98d5f713044faf3b54804a6512
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-07-26 09:54:32 -0300

    Add link to customer area

--- pages/index.tsx
+++ pages/index.tsx
@@ -76,6 +76,12 @@ export default function Home() {
         <h1 className={styles.title}>
           Estamos construindo um novo site para você.
         </h1>
+        <div className={styles.customer_area_link}>
+          <p>
+            Central do Assinante?
+            <a href='http://68.183.119.146/radiusnet/cda'> Clique aqui.</a>
+          </p>
+        </div>
         <div className={styles.buttons_container}>
           <a
             href='https://wa.me/5519999749477?text=Olá,%20tudo%20bem?'
