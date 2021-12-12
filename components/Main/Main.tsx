f9540024c1d35a747e11cbb446be242ff677f13c
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2021-12-12 15:03:27 -0300

    move card rendering to card container

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -2,16 +2,11 @@ import Image from 'next/image'
 import Row from '@/components/Row/Row'
 import Container from '@/components/Container/Container'
 import CardContainer from '@/components/CardContainer/CardContainer'
-import Card from '@/components/Card/Card'
 import ButtonsContainer from '@/components/ButtonsContainer/ButtonsContainer'
-import LinkButton from '@/components/LinkButton/LinkButton'
 import smile from '@/images/smile.jpg'
 import woman from '@/images/woman.jpg'
 import man from '@/images/img3.jpg'
 import hoe from '@/images/img2.jpg'
-import card1 from '@/images/cards/card1.jpg'
-import card2 from '@/images/cards/card2.jpg'
-import card3 from '@/images/cards/card3.jpg'
 
 import styles from './Main.module.css'
 
@@ -44,29 +39,7 @@ export default function Main() {
                 </div>
               </div>
             </div>
-            <CardContainer>
-              <Card
-                imageDescription='man sitting in field, looking to mountains'
-                imagePath={card2}
-                text='Escolha o plano perfeito para atender as suas necessidades!
-                  Temos tecnologia para oferecer a você internet com alta
-                  velocidade, estável e de qualidade!'
-              />
-              <Card
-                imageDescription='plants being irrigated'
-                imagePath={card1}
-                text='Mora ou trabalha na fazenda e não tem acesso fácil a internet?
-                  A Wiss Telecom leva para você a qualidade e a tecnologia
-                  da cidade!'
-              />
-              <Card
-                imageDescription='Woman using a computer on an office'
-                imagePath={card3}
-                text='Para estudar, trabalhar ou lazer, a Wiss Telecom tem internet
-                  rural rápida, estável e por um preço super acessível. É
-                  tecnologia de ponta para o campo!'
-              />
-            </CardContainer>
+            <CardContainer />
           </Container>
         </section>
         <section className={`${styles.section} ${styles.section2}`}>
