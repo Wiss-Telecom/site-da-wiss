91ea40e916d50f4f3ce4b8884455e4fce257d43e
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2022-01-13 23:15:22 -0300

    remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
-import styles from '../styles/Home.module.css'
 import Header from '@/components/Header/Header'
-import Footer from '@/components/Footer/Footer'
 import Main from '@/components/Main/Main'
-import Navbar from '@/components/Navbar/Navbar'
 import ContactForm from '@/components/ContactForm/ContactForm'
 
 export default function Home() {
