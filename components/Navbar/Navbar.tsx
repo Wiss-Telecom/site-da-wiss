f0b02974ce9ce8d1b73117d58c6d3bd458cf4ff4
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2022-03-16 20:09:23 -0300

    set Navbar style with tailwind

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 
 import Link from 'next/link'
 
-import Row from '@/components/Row/Row'
 import Container from '@/components/Container/Container'
 import Logo from '@/components/Logo/Logo'
 
@@ -16,65 +15,63 @@ export default function Navbar() {
   }
 
   return (
-    <Row>
-      <div className={styles.navWrapper}>
-        <Container>
-          <div className={styles.navContainer}>
-            <div className={styles.logoContainer}>
-              <a href='#top'>
-                <Logo />
-              </a>
-            </div>
-            <div className={styles.menuButton} onClick={handleMenuClick}>
-              <span
-                className={`${styles.iconBar} ${
-                  isMenuOpen ? styles.activeIconBar : ''
-                }`}
-              ></span>
-              <span
-                className={`${styles.iconBar} ${
-                  isMenuOpen ? styles.activeIconBar : ''
-                }`}
-              ></span>
-              <span
-                className={`${styles.iconBar} ${
-                  isMenuOpen ? styles.activeIconBar : ''
-                }`}
-              ></span>
-            </div>
-            <div
-              className={`${styles.bg} ${isMenuOpen ? styles.bgShow : ''}`}
-              onClick={handleMenuClick}
-            ></div>
-            <nav
-              className={`${styles.navContent} ${
-                isMenuOpen ? styles.navContentShow : ''
-              }`}
-            >
-              <ul className={styles.navList}>
-                <li onClick={handleMenuClick}>
-                  <Link href='/#top'>
-                    <a>Home</a>
-                  </Link>
-                </li>
-                <li onClick={handleMenuClick}>
-                  <Link href='/#contact'>
-                    <a>Contato</a>
-                  </Link>
-                </li>
-                <li onClick={handleMenuClick}>
-                  <a
-                    className={styles.linkButton}
-                    href='https://radiusnet.wisstelecom.com.br/radiusnet/cda/login.php'
-                  >
-                    Central do Assinante
-                  </a>
-                </li>
-              </ul>
-            </nav>
+    <div className={styles.navWrapper}>
+      <Container>
+        <div className={styles.navContainer}>
+          <div className={styles.logoContainer}>
+            <a href='#top'>
+              <Logo />
+            </a>
           </div>
-        </Container>
-      </div>
-    </Row>
+          <div className={styles.menuButton} onClick={handleMenuClick}>
+            <span
+              className={`${styles.iconBar} ${
+                isMenuOpen ? styles.activeIconBar : ''
+              }`}
+            ></span>
+            <span
+              className={`${styles.iconBar} ${
+                isMenuOpen ? styles.activeIconBar : ''
+              }`}
+            ></span>
+            <span
+              className={`${styles.iconBar} ${
+                isMenuOpen ? styles.activeIconBar : ''
+              }`}
+            ></span>
+          </div>
+          <div
+            className={`${styles.bg} ${isMenuOpen ? styles.bgShow : ''}`}
+            onClick={handleMenuClick}
+          ></div>
+          <nav
+            className={`${styles.navContent} ${
+              isMenuOpen ? styles.navContentShow : ''
+            }`}
+          >
+            <ul className={styles.navList}>
+              <li onClick={handleMenuClick}>
+                <Link href='/#top'>
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li onClick={handleMenuClick}>
+                <Link href='/#contact'>
+                  <a>Contato</a>
+                </Link>
+              </li>
+              <li onClick={handleMenuClick}>
+                <a
+                  className={styles.linkButton}
+                  href='https://radiusnet.wisstelecom.com.br/radiusnet/cda/login.php'
+                >
+                  Central do Assinante
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </Container>
+    </div>
   )
 }
