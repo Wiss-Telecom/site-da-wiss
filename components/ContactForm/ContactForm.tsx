b764e7f2ec2d5fb4ab4ba08692888cb8f6eeaff6
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2022-03-17 08:38:18 -0300

    add tailwind classes

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -3,7 +3,6 @@ import { useState, useRef } from 'react'
 import HCaptcha from '@hcaptcha/react-hcaptcha'
 
 import Container from '@/components/Container/Container'
-import Row from '@/components/Row/Row'
 
 import man from '@/images/img3.jpg'
 import sentMail from '@/images/sent-mail.svg'
@@ -63,79 +62,71 @@ export default function ContactForm() {
   }
 
   return (
-    <Row>
-      <section id='contact' className={styles.contact}>
-        <Container>
-          <div className={styles.contactContent}>
-            <div className={styles.contactImageContainer}>
-              <Image src={man} alt='man with PC on desk' layout='responsive' />
-            </div>
-            <div>
-              <h2 className={styles.formTitle}>Fale conosco</h2>
-              {sending ? (
-                <div className={styles.sentMessage}>
-                  <Image src={spinner} alt='spinner' layout='responsive' />
-                </div>
-              ) : submitted ? (
-                <div>
-                  <h3>Obrigado</h3>
-                  <div className={styles.sentMessage}>
-                    <Image
-                      src={sentMail}
-                      alt='icon of an envelope'
-                      layout='responsive'
-                    />
-                  </div>
-                  <p>Retornaremos o contato em breve</p>
-                </div>
-              ) : (
-                <form className={styles.form} onSubmit={handleSubmit}>
-                  <input
-                    id='name'
-                    type='text'
-                    autoComplete='name'
-                    placeholder='Seu nome'
-                    aria-label='Seu nome'
-                    required
-                    className={styles.inputField}
-                    onChange={(event) => setName(event.target.value)}
-                  />
-                  <input
-                    id='email'
-                    type='email'
-                    placeholder='Seu e-mail'
-                    aria-label='Seu e-mail'
-                    required
-                    className={styles.inputField}
-                    onChange={(event) => setEmail(event.target.value)}
-                  />
-                  <textarea
-                    id='message'
-                    name='message'
-                    placeholder='Sua mensagem'
-                    aria-label='Sua mensagem'
-                    className={styles.inputField}
-                    required
-                    onChange={(event) => setMessage(event.target.value)}
-                  ></textarea>
-                  <HCaptcha
-                    ref={hcaptchaRef}
-                    sitekey={
-                      process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY as string
-                    }
-                    onVerify={onHCaptchaChange}
-                  />
-                  <input
-                    type='submit'
-                    value='Enviar'
-                    className={styles.submit}
-                  />
-                </form>
-              )}
-            </div>
+    <section id='contact' className={styles.contact}>
+      <Container>
+        <div className={styles.contactContent}>
+          <div className={styles.contactImageContainer}>
+            <Image src={man} alt='man with PC on desk' layout='responsive' />
           </div>
-        </Container>
-      </section>
-    </Row>
+          <div>
+            <h2 className={styles.formTitle}>Fale conosco</h2>
+            {sending ? (
+              <div className={styles.sentMessage}>
+                <Image src={spinner} alt='spinner' layout='responsive' />
+              </div>
+            ) : submitted ? (
+              <div>
+                <h3>Obrigado</h3>
+                <div className={styles.sentMessage}>
+                  <Image
+                    src={sentMail}
+                    alt='icon of an envelope'
+                    layout='responsive'
+                  />
+                </div>
+                <p>Retornaremos o contato em breve</p>
+              </div>
+            ) : (
+              <form className={styles.form} onSubmit={handleSubmit}>
+                <input
+                  id='name'
+                  type='text'
+                  autoComplete='name'
+                  placeholder='Seu nome'
+                  aria-label='Seu nome'
+                  required
+                  className={styles.inputField}
+                  onChange={(event) => setName(event.target.value)}
+                />
+                <input
+                  id='email'
+                  type='email'
+                  placeholder='Seu e-mail'
+                  aria-label='Seu e-mail'
+                  required
+                  className={styles.inputField}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+                <textarea
+                  id='message'
+                  name='message'
+                  placeholder='Sua mensagem'
+                  aria-label='Sua mensagem'
+                  className={styles.inputField}
+                  required
+                  onChange={(event) => setMessage(event.target.value)}
+                ></textarea>
+                <HCaptcha
+                  ref={hcaptchaRef}
+                  sitekey={process.env.NEXT_PUBLIC_HCAPTCHA_SITE_KEY as string}
+                  onVerify={onHCaptchaChange}
+                />
+                <input type='submit' value='Enviar' className={styles.submit} />
+              </form>
+            )}
+          </div>
+        </div>
+      </Container>
+    </section>
   )
 }
