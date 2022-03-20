b24e67b5d13bd199a9004f7b7a1d9b9304616977
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2022-03-20 13:43:42 -0300

    refactor simplify code

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image'
-import Row from '@/components/Row/Row'
 import Container from '@/components/Container/Container'
 import CardContainer from '@/components/CardContainer/CardContainer'
 import ButtonsContainer from '@/components/ButtonsContainer/ButtonsContainer'
@@ -14,64 +13,52 @@ export default function Main() {
     <main className={styles.main}>
       <ButtonsContainer />
       <Intro />
-      <Row>
+      <CardContainer />
+      <section className={styles.section}>
         <Container>
-          <CardContainer />
+          <div className={`${styles.sectionContent} ${styles.section1Content}`}>
+            <div className={styles.imageContainer}>
+              <Image src={woman} alt='a woman on farm' layout='responsive' />
+            </div>
+            <div>
+              <p>
+                A Wiss Telecom nasceu para oferecer atendimento de qualidade aos
+                moradores da zona rural.
+              </p>
+              <p>
+                Nosso objetivo é levar a todos acesso à Internet de excelência,
+                colaborando para a modernização do campo e democratização do
+                acesso à Internet.
+              </p>
+            </div>
+          </div>
         </Container>
-      </Row>
-      <Row>
-        <section className={`${styles.section} ${styles.section2}`}>
-          <Container>
-            <div
-              className={`${styles.sectionContent} ${styles.section1Content}`}
-            >
-              <div className={styles.imageContainer}>
-                <Image src={woman} alt='a woman on farm' layout='responsive' />
-              </div>
-              <div>
-                <p>
-                  A Wiss Telecom nasceu para oferecer atendimento de qualidade
-                  aos moradores da zona rural.
-                </p>
-                <p>
-                  Nosso objetivo é levar a todos acesso à Internet de
-                  excelência, colaborando para a modernização do campo e
-                  democratização do acesso à Internet.
-                </p>
-              </div>
+      </section>
+      <section className={`${styles.section} ${styles.section2}`}>
+        <Container>
+          <div className={`${styles.sectionContent} ${styles.section2Content}`}>
+            <div className={styles.imageContainer}>
+              <Image
+                src={hoe}
+                alt='man with a hoe on field'
+                layout='responsive'
+              />
             </div>
-          </Container>
-        </section>
-      </Row>
-      <Row>
-        <section className={`${styles.section} ${styles.section3}`}>
-          <Container>
-            <div
-              className={`${styles.sectionContent} ${styles.section2Content}`}
-            >
-              <div className={styles.imageContainer}>
-                <Image
-                  src={hoe}
-                  alt='man with a hoe on field'
-                  layout='responsive'
-                />
-              </div>
-              <div>
-                <p>
-                  Com planos sem franquia e com foco total em áreas rurais,
-                  trabalhamos para levar até você
-                </p>
-                <ul className={styles.advantagesList}>
-                  <li>Mais velocidades</li>
-                  <li>Mais estabilidade</li>
-                  <li>Valores amigáveis</li>
-                  <li>Atendimento personalizado</li>
-                </ul>
-              </div>
+            <div>
+              <p>
+                Com planos sem franquia e com foco total em áreas rurais,
+                trabalhamos para levar até você
+              </p>
+              <ul className={styles.advantagesList}>
+                <li>Mais velocidades</li>
+                <li>Mais estabilidade</li>
+                <li>Valores amigáveis</li>
+                <li>Atendimento personalizado</li>
+              </ul>
             </div>
-          </Container>
-        </section>
-      </Row>
+          </div>
+        </Container>
+      </section>
     </main>
   )
 }
